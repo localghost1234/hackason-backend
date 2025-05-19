da4683cab6ea71b335b691ec46fc09878226ae58
ghost <viczaz99@hotmail.com>
2025-05-18 21:50:18 -0700

    Initial Commit
    Copied code from Spring Initializr

---
+++ src/main/java/com/desiertofirewall/hackason/backend/HackasonApplication.java
@@ -0,0 +1,13 @@
+package com.desiertofirewall.hackason.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackasonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desiertofirewall/hackason/backend/HackasonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desiertofirewall.hackason.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
